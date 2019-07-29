382308ee1a99513efa4216760bda66b94072b1ac
Victor Parmar <vic@smalldata.tech>
2019-07-29 19:29:46 +0200

    F update backup script

--- scripts/backup-ebackup.sh
+++ scripts/backup-ebackup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rsync -Pav --delete-after ~/nextcloud/ /media/victor/ebackup/nextcloud
+rsync -Pav --delete-after ~/db-backups/ /media/victor/ebackup/db-backups
 rsync -Pav --delete-after --exclude="node_modules" ~/npq/ /media/victor/ebackup/npq
 rsync -Pav --delete-after /media/victor/ddd/pics/ /media/victor/ebackup/pics
 
