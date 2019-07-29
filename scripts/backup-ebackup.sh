cb93248727149d14e16d252868699de57138abf6
Victor Parmar <vic@smalldata.tech>
2019-07-29 21:16:53 +0200

    F add edu to backup

--- scripts/backup-ebackup.sh
+++ scripts/backup-ebackup.sh
@@ -4,4 +4,5 @@ rsync -Pav --delete-after ~/nextcloud/ /media/victor/ebackup/nextcloud
 rsync -Pav --delete-after ~/db-backups/ /media/victor/ebackup/db-backups
 rsync -Pav --delete-after --exclude="node_modules" ~/npq/ /media/victor/ebackup/npq
 rsync -Pav --delete-after /media/victor/ddd/pics/ /media/victor/ebackup/pics
+rsync -Pav --delete-after /media/victor/ddd/edu/ /media/victor/ebackup/edu
 
