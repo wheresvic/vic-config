a750fbe224f22c630189a049c099221f742ec666
Victor Parmar <vic@smalldata.tech>
2019-07-29 16:28:51 +0200

    F add 2 way sync

--- scripts/pi-rsync.sh
+++ scripts/pi-rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -Pav /media/victor/ddd/videos/movies/2-vic/ pi@192.168.42.178:/media/pi/bbb/bbb/video/movies
+rsync -Pav --delete-after /media/victor/ddd/videos/movies/2-vic/ pi@192.168.42.178:/media/pi/bbb/bbb/video/movies
 
